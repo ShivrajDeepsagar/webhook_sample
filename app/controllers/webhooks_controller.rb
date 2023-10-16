f8ab107354c21c09bf5cae81e5c153273c9dccf7
Shivraj Deepsagar <shivrajdeepsagar@Shivrajs-MacBook-Air.local>
2023-10-16 07:57:27 +0530

    webhook controller changes

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,2 +1,36 @@
 class WebhooksController < ApplicationController
+
+  POSTMAN_ECHO_URL = 'https://postman-echo.com/post'
+
+  def index
+    @webhooks = Webhook.all
+  end
+
+  def create
+    @webhook = Webhook.new(webhook_params)
+byebug
+    if @webhook.save
+      send_post_request_to_postman_echo(@webhook.url, @webhook.payload)
+      render json: { message: 'Webhook created successfully and sent to Postman Echo' }, status: :created
+    else
+      render json: { errors: @webhook.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  #private
+
+  def webhook_params
+    byebug
+    params.require(:webhook).permit(:url, :payload)
+  end
+
+  def send_post_request_to_postman_echo(url, payload)
+    byebug
+    response = RestClient.post(POSTMAN_ECHO_URL, payload.to_json, content_type: :json)
+    puts "Postman Echo Response Code: #{response.code}"
+    puts "Postman Echo Response Body: #{response.body}"
+  rescue RestClient::ExceptionWithResponse => e
+    puts "Error: #{e.response.code} - #{e.response.body}"
+  end
+
 end
